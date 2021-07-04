80babb90202631c4bef3fb4b4fc246a97308bd7d
uzza9 <chang.jeon99@gmail.com>
2021-07-03 20:22:25 -0500

    Initial commit

---
+++ app/src/main/java/com/bl/chat_2/ChannelActivity.kt
@@ -0,0 +1,11 @@
+package com.bl.chat_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChannelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_channel)
+    }
+}
